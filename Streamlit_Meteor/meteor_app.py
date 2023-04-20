20674918d2994e145df097e8f1825c3a9a63cd24
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2023-04-20 14:40:13 -0700

    Update meteor_app.py

--- Streamlit_Meteor/meteor_app.py
+++ Streamlit_Meteor/meteor_app.py
@@ -4,8 +4,13 @@ import pandas as pd
 # read data
 meteor_CSV = pd.read_csv('./Data/meteorite-landings.csv')
 df =  meteor_CSV.copy()
-numeric_df = df.select_dtypes(include=["float32","float64","int32","int64"])
+# clean data
+df = df[(df['reclong']<=180) & (df['reclong']>=-180) ] # should only be btwn -180 to 180
+df = df[(df['reclat']!= 0) | (df['reclong']!= 0)]      # can't have lat or long of 0
+df = df.dropna() # removes rows with any missing col
+df.isnull().sum() # checks the number of missing values in each column 
 
+numeric_df = df.select_dtypes(include=["float32","float64","int32","int64"])
 subtitles = ['Raw Meteor Data', 'Description', 'Correlation', 'Covariance']
 subaction = [df, df.describe(), numeric_df.corr(), numeric_df.cov()]   
 
@@ -13,7 +18,6 @@ with st.sidebar:
     st.title('Display')
     side_radio = st.radio(
         "Select your data",
-
         (subtitle for subtitle in subtitles)
     )
 
