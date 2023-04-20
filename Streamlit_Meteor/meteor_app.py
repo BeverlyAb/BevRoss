a1ac50abe9bc2e8c4d9d82ffff6e278afade8092
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2023-04-20 14:32:00 -0700

    Update meteor_app.py

--- Streamlit_Meteor/meteor_app.py
+++ Streamlit_Meteor/meteor_app.py
@@ -6,18 +6,19 @@ meteor_CSV = pd.read_csv('./Data/meteorite-landings.csv')
 df =  meteor_CSV.copy()
 numeric_df = df.select_dtypes(include=["float32","float64","int32","int64"])
 
+subtitles = ['Raw Meteor Data', 'Description', 'Correlation', 'Covariance']
+subaction = [df, df.describe(), numeric_df.corr(), numeric_df.cov()]   
 
-st.title('Raw Meteor Data')
-df
-st.subheader('Description')
-st.write(df.describe())
-c1, c2 = st.columns(2)
-with c1:
-    st.subheader('Correlation')
-    st.write(numeric_df.corr())
-with c2:
-    st.subheader('Covariance')
-    st.write(numeric_df.cov())
+with st.sidebar:
+    st.title('Display')
+    side_radio = st.radio(
+        "Select your data",
+
+        (subtitle for subtitle in subtitles)
+    )
 
 
-
+for i in range(len(subtitles)):
+    if side_radio == subtitles[i]:
+        st.subheader(subtitles[i])
+        st.write(subaction[i])
